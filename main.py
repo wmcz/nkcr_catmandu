19f42f516d6dd750b5e0c4b4b01fa2045b15d4b1
Jiri Sedlacek <jirisedlacek@gmail.com>
2023-01-13 11:12:55 +0100

    not used benediktinka (Q97772204) in occupation

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ count_first_step = 0
 count_second_step = 0
 limit = 50000
 
-occupations_not_used_in_occupation_because_is_in_function = ['Q103163', 'Q29182', 'Q611644', 'Q102039658', 'Q212071', 'Q22132694', 'Q63970319', 'Q11165895', 'Q83460']
+occupations_not_used_in_occupation_because_is_in_function = ['Q103163', 'Q29182', 'Q611644', 'Q102039658', 'Q212071', 'Q22132694', 'Q63970319', 'Q11165895', 'Q83460', 'Q97772204']
 fields_of_work_not_used_in_field_of_work_because_is_not_ok = ['Q11214']
 
 parser = argparse.ArgumentParser(description='NKČR catmandu pipeline.')
