4b87309683c2cb71bb726f3fb41343379212f99b
Jiri Sedlacek <jirisedlacek@gmail.com>
2024-01-16 11:08:43 +0100

    run only import

---
+++ pipeline_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Move to directory"
+cd /home/frettie/nkcr_catmandu_pipeline;
+. /home/frettie/nkcr_catmandu_pipeline/bin/activate && python3 /home/frettie/nkcr_catmandu_pipeline/main.py --input /home/frettie/nkcr_catmandu_pipeline/output.csv ; deactivate
+
