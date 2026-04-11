4d643feecc561181597b3470fcb652d88057213c
Jiri Sedlacek <jirisedlacek@gmail.com>
2026-04-11 21:53:34 +0200

    fix pipeline run

--- pipeline_run.sh
+++ pipeline_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Move to directory"
 cd /home/frettie/nkcr_catmandu_pipeline;
-. /home/frettie/nkcr_catmandu_pipeline/bin/activate && python3 /home/frettie/nkcr_catmandu_pipeline/main.py --input /home/frettie/nkcr_catmandu_pipeline/output.csv ; deactivate
+#. /home/frettie/nkcr_catmandu_pipeline/bin/activate && python3 /home/frettie/nkcr_catmandu_pipeline/main.py --input /home/frettie/nkcr_catmandu_pipeline/output.csv ; deactivate
+. /Users/jirisedlacek/htdocs/nkcr_catmandu/venv/bin/activate && python3 /Users/jirisedlacek/htdocs/nkcr_catmandu/main.py --input /Users/jirisedlacek/htdocs/nkcr_catmandu/output.csv ; deactivate
 
